c501d5959c78fb7cd8d478d2d7df4f31f8266547
sakun9526 <sakunrathnayaka@yahoo.com>
2020-12-11 11:55:07 +0530

    configure tailwind css done

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+@section('content')
+  <h1>Index</h1>
+@endsection
+
---
+++ resources/views/posts/second.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+
+@section('aftercontent')
+<h1>this is after content</h1>    
+@endsection
