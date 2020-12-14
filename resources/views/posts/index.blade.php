d587357b7d6004bd37e8193b95e2bb82ff628bad
sakun9526 <sakunrathnayaka@yahoo.com>
2020-12-14 13:33:52 +0530

    post pagination done

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -31,6 +31,7 @@
                         <p class="mb-2">{{$post->body}}</p>
                     </div>
                 @endforeach
+                {{$posts->links()}}
             @else
                 <p>There are no posts at the moment!!</p>
             @endif
